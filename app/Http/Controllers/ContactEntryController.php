78a97e6e4fd17572e434a608978942329f8ad0f4
Alisa_Vynohradova <a.vynohradova@vehikl.com>
2020-08-05 20:29:19 -0400

    Set up basic routes

---
+++ app/Http/Controllers/ContactEntryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactEntryController extends Controller
+{
+    public function send()
+    {
+        return 123;
+    }
+}
