110b21a8a6c1cc3b04d99f3b4245cdc45fef8616
Alisa_Vynohradova <a.vynohradova@vehikl.com>
2020-08-21 16:59:35 -0400

    Set up sendMessage function

--- app/ContactEntry.php
+++ app/ContactEntry.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContactEntry extends Model
 {
-    //
+    public function sendMessage()
+    {
+        $this->date_contacted = date('Y-m-d');
+        $this->save();
+    }
 }
