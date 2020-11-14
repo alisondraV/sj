bafdc25a7c80f9f2f69abc27cdb4c15ac6c7dcd3
alisondraV <alice.7414122013@gmail.com>
2020-11-14 13:39:50 -0500

    Import css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script defer src="{{ mix('js/app.js') }}"></script>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <title>SJ's Painting</title>
     </head>
