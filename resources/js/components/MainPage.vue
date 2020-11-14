0029e22396e456083f2409a0e43f6ab103714968
alisondraV <alice.7414122013@gmail.com>
2020-11-14 15:38:08 -0500

    Set up aliases and bring in scrolling

--- resources/js/components/MainPage.vue
+++ resources/js/components/MainPage.vue
@@ -1,13 +1,17 @@
 <template>
-    <div class="font-bold">
-        Hello
+    <div class="h-screen flex justify-center items-center bg-black">
+        <div class="text-white">SJ's<br> Painting</div>
+        <div class="text-white">Let the Euro do it</div>
+        <div>
+            <img class="w-1/2" src="@assets/Bin.svg"/>
+            <img class="animate w-1/4" src="@assets/Roller.svg"/>
+        </div>
     </div>
 </template>
 
-<script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
-</script>
+
+<style scoped>
+.animate {
+    animation: bounceOutDown 3s 1s 1, bounceInUp 3s 3s 1;
+}
+</style>
