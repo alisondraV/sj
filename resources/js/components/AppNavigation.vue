57d808ae40a6c31cf019e7fcafc42b92e098b4fa
alisondraV <alice.7414122013@gmail.com>
2020-11-14 16:01:34 -0500

    Update navigation styling

--- resources/js/components/AppNavigation.vue
+++ resources/js/components/AppNavigation.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="bg-black flex">
-        <div class="flex-1 flex flex-row items-center">
-            <img class="w-20 mx-3" src="@assets/Logo.png"/>
-            <a href="tel:12266008629" class="text-white">+1 (226) 600-86-29</a>
+    <nav class="bg-black flex p-3 fixed w-screen">
+        <div class="flex-1 flex flex-row items-center md:ml-12">
+            <img class="w-20 mx-4" src="@assets/Logo.png"/>
+            <a href="tel:12266008629" class="text-white text-lg">+1 (226) 600-86-29</a>
         </div>
         <div class="flex-1 flex justify-between items-center mr-8">
             <a
                 v-for="item in items"
                 :href="item.href"
-                class="rounded p-2 text-white cursor-pointer hover:bg-gray-700"
+                class="rounded p-2 text-white text-xl cursor-pointer hover:bg-gray-700"
             >
                 {{ item.title }}
             </a>
         </div>
-    </div>
+    </nav>
 </template>
 
 <script>
