e430349f3b2d443f28162a838a71907e4b361d54
alisondraV <alice.7414122013@gmail.com>
2020-11-17 09:53:53 -0500

    Update mail template

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,4 +1,4 @@
-<p><b>{{$entry->name}}</b> has send you a message</p>
-<p>Contact email is <b>{{$entry->email}}</b></p>
-<p>Request:</p>
+<p><h3>{{$entry->name}}</h3> has send you a message</p>
+<p><h3>Contact email</h3> is <b>{{$entry->email}}</b></p>
+<h3>Request:</h3>
 <p>{{$entry->message}}</p>
