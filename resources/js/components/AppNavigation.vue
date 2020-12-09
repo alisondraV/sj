57a3a7219b4e5a32958632b1f241d2f4a423adb8
alisondraV <alice.7414122013@gmail.com>
2020-12-08 19:36:07 -0500

    Add mobile-friendly navbar

--- resources/js/components/AppNavigation.vue
+++ resources/js/components/AppNavigation.vue
@@ -4,14 +4,22 @@
             <img class="w-20 mx-4" src="@assets/Logo.png"/>
             <a href="tel:12266008629" class="text-white text-lg">+1 (226) 600-86-29</a>
         </div>
-        <div class="flex-1 flex justify-between items-center mr-8">
-            <a
-                v-for="item in items"
-                :href="item.href"
-                class="rounded p-2 text-white text-xl cursor-pointer hover:bg-gray-700"
+        <div class="md:flex md:items-center">
+            <div class="cursor-pointer block md:hidden" @click="toggleMenu">
+                <img class="w-8 m-8" src="@assets/Menu.svg"/>
+            </div>
+            <div
+                :class="isOpen ? 'block' : 'hidden'"
+                class="md:flex-1 md:flex md:h-auto absolute right-0 h-screen bg-black justify-between items-center px-8"
             >
-                {{ item.title }}
-            </a>
+                <a
+                    v-for="item in items"
+                    :href="item.href"
+                    class="md:mx-3 block rounded p-2 text-white text-xl cursor-pointer hover:bg-gray-700"
+                >
+                    {{ item.title }}
+                </a>
+            </div>
         </div>
     </nav>
 </template>
@@ -19,13 +27,19 @@
 <script>
 export default {
     data: () => ({
-        drawer: false,
+        isOpen: false,
         items: [
-            { href: '#about', title: 'About Us' },
-            { href: '#why', title: 'Why Us?' },
-            { href: '#works', title: 'Our Works' },
-            { href: '#contact', title: 'Contact Us' }
+            {href: '#about', title: 'About Us'},
+            {href: '#why', title: 'Why Us?'},
+            {href: '#works', title: 'Our Works'},
+            {href: '#contact', title: 'Contact Us'}
         ]
-    })
+    }),
+    methods: {
+        toggleMenu() {
+            this.isOpen = !this.isOpen
+        }
+    }
 }
 </script>
+
