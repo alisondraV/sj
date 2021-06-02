e7c693d61699237e47bbae7c2b9e343036575a6d
Daniel Mills <daniel.mills@ableto.com>
2021-06-02 15:30:43 -0400

    Remove some stuff

--- resources/js/components/AboutUs.vue
+++ resources/js/components/AboutUs.vue
@@ -1,17 +1,13 @@
 <template>
     <div class="min-h-screen flex lg:flex-row flex-col items-center px-10 mb-24 md:px-20">
         <div class="flex-1 flex justify-center mt-24 md:mr-6 lg:mt-4 md:w-1/2">
-            <img src="@assets/About.png" alt="About Us"/>
+<!--            <img src="@assets/About.png" alt="About Us"/>-->
         </div>
         <div class="flex-1">
             <p class="text-4xl md:text-6xl tracking-widest lg:text-right text-center md:m-4 md:mt-18 mt-4">
                 About Us
             </p>
             <p class="text-lg">
-                Hey! My name is Shkelqim Jairi. Painting and renovation have become my passion since 2014.
-                I’ve always been around it and I truly enjoy improving things around me.
-                That’s why, I’ve decided to open my own business and help other people to
-                transform their properties.<br><br>
                 I always take pride in my work and don’t finish until the result is perfect! My team uses the best
                 tools on the market, so we can guarantee high quality within shortest amount of time.<br><br>
                 <b>Now, I’m here to help YOU!</b>
